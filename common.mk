rk134/android_device_realme_sm8350-common

sm8350-common: Import yupik post_boot script in-tree
  * for further edits

Change-Id: I7ed4b8ed0c96b880e44939792f111b2f1ba06072
Signed-off-by: rk134 <rahul.kantrapally@gmail.com>

common.mk
@@ -238,6 +238,7 @@ PRODUCT_PACKAGES += \
     init.oplus.rc \
     init.oplus.sh \
     init.qcom.early_boot.sh \
+    init.kernel.post_boot-lahaina.sh \
     init.qcom.rc \
     init.qcom.recovery.rc \
     init.qcom.sh \

Quallenauge/android_device_oplus_sm8350-common

common.mk
@@ -229,6 +229,7 @@ PRODUCT_PACKAGES += \
     fstab.default \
     fstab.default.vendor_ramdisk \
     init.class_main.sh \
+    init.kernel.post_boot-yupik.sh \
     init.oplus.hw.rc \
     init.oplus.hw.rc.recovery \
     init.oplus.rc \